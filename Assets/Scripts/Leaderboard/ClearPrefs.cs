774d7194a4f592fa1c697c82c0da5b11b33df79a
WANG Kaixun <12310803@mail.sustech.edu.cn>
2025-07-17 14:40:10 +0800

    clear prefs

---
+++ Assets/Scripts/Leaderboard/ClearPrefs.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
+    }
+}
